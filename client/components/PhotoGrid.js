94b2b3bf0471846a3575e1d919c1fb1141ad3a99
Vinay Swaminathan <Vinay.swami91@gmail.com>
2017-05-12 15:00:18 +0530

    Set up routing for all components

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-
-class PhotoGrid extends React.Component{
-	render(){
-		return (
-				<div className="photo-grid">
-						Me be photo grid
-				</div>
-			)
-	}
-};
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="photo-grid">
+      I'm the photo grid
+      </div>
+    )
+  }
+});
 
 export default PhotoGrid;
